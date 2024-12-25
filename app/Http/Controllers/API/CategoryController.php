a0830bc403d26971064895a595545dc9abc0ae1b
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2024-12-25 20:53:55 +0500

    First Commit

---
+++ app/Http/Controllers/API/CategoryController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    
+
+    public function index()
+    {
+        $categories = Category::paginate(10);
+
+        // return $categories;
+        $data = [
+            'models' => $categories,
+            'message' => 'success'
+        ];
+
+        return response()->json($data);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate(['name' => 'required']);
+
+        $category = Category::create($request->all());
+
+        $data = [
+            'model' => $category,
+            'message' => 'success'
+        ];
+
+        return response()-> json($data);
+    }
+
+    public function show(Category $category)
+    {
+
+        $data = [
+            'model' => $category,
+            'message' => 'success'
+        ];
+
+        return response()-> json($data);
+    }
+
+    public function update(Request $request, Category $category)
+    {
+        $request->validate(['name' => 'required']);
+
+        $category->update($request->all());
+
+        $data = [
+            'model' => $category,
+            'message' => 'success'
+        ];
+
+        return response()-> json($data);
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        $data = [
+            'message' => 'success'
+        ];
+
+        return response()-> json($data);
+    }
+}
